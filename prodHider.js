961681c62c5b19d05a88eb732d00d3bd21a0b26c
Takuan <91788582+TKsStuff@users.noreply.github.com>
2023-06-05 18:52:24 +0200

    Add files via upload

---
+++ prodHider.js
@@ -0,0 +1,4 @@
+function hide(ApexOption){
+	var el = document.getElementsByClassName(ApexOption)
+	el.style.display = 'none';
+}
---
+++ slutProjektJscript.js
@@ -0,0 +1,4 @@
+	var count = 0;
+	var inc = 0;
+	var slider = document.getElementsByClassName("ProdBar")[0];
+	var itemDisplay = 3;
