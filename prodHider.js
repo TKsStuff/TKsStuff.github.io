5a3a120e123c720fe6a03e432e259870e8a48191
Takuan <91788582+TKsStuff@users.noreply.github.com>
2023-06-05 22:19:13 +0200

    Update prodHider.js

--- prodHider.js
+++ prodHider.js
@@ -1,5 +1,15 @@
-function hideProducts(){
-document.getElementsByClassName("")
+function showR6(){
+document.getElementById('ApexOptions').style.display = 'none';
+document.getElementById('LOLOptions').style.display = 'none';
 
+}
+function showApex(){
+document.getElementById('R6Options').style.display = 'none';
+document.getElementById('LOLOptions').style.display = 'none';
+
+}
+function showLol(){
+document.getElementById('ApexOptions').style.display = 'none';
+document.getElementById('R6Options').style.display = 'none';
 
 }
